bbccbb07b9a342a3844d7279007ef3500faa754b
Minh Duc <ductm@devblock.net>
2021-02-18 11:59:51 +0700

    Update NotFound container

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -7,15 +7,19 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
+import Container from 'react-bootstrap/Container';
 import H1 from 'components/H1';
 import messages from './messages';
+import Wrapper from './Wrapper';
 
 export default function NotFound() {
   return (
-    <article>
-      <H1>
-        <FormattedMessage {...messages.header} />
-      </H1>
-    </article>
+    <Container>
+      <Wrapper>
+        <H1 className="text-center">
+          <FormattedMessage {...messages.header} />
+        </H1>
+      </Wrapper>
+    </Container>
   );
 }
